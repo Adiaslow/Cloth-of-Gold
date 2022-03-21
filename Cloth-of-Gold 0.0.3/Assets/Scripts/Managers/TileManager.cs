26b1797baf60b922b27d73875df686044a822da8
Adam Murray <a.murray0413@gmail.com>
2022-03-21 09:19:34 -0700

    primitive chunk system

--- Cloth-of-Gold 0.0.3/Assets/Scripts/Managers/TileManager.cs
+++ Cloth-of-Gold 0.0.3/Assets/Scripts/Managers/TileManager.cs
@@ -9,8 +9,13 @@ public class TileManager : MonoBehaviour
 
     private List<Tile> _tiles = new List<Tile>();
 
-    public int WIDTH = 10;
-    public int HEIGHT = 10;
+    public int chunkCount = 2;
+    public int CHUNK_SIZE = 8;
+
+    public int WIDTH;
+    public int HEIGHT;
+
+    public int TILE_COUNT = 0;
 
     [SerializeField] private float scale = 1;
     [SerializeField] private float heightMultiplier = 100;
@@ -22,12 +27,14 @@ public class TileManager : MonoBehaviour
 
     private void Start()
     {
-        CreateTiles();
+        GetWidthAndHeight();
+        CreateChunks();
+        // CreateTiles();
     }
 
     private void Update()
     {
-        UpdateTileType();
+        // UpdateTileType();
     }
 
     private void CreateTiles()
@@ -55,6 +62,99 @@ public class TileManager : MonoBehaviour
                 _tiles[_tileIndex].transform.Translate(new Vector2(xPos, yPos));
 
                 _tileIndex++;
+            }
+        }
+    }
+
+    private void GetWidthAndHeight()
+    {
+        if (chunkCount == 0)
+        {
+            WIDTH = HEIGHT = 0;
+        }
+
+        else if (chunkCount % 2 != 0)
+        {
+            WIDTH = CHUNK_SIZE * chunkCount;
+        }
+
+        else if (chunkCount % 2 == 0)
+        {
+            WIDTH = CHUNK_SIZE * chunkCount;
+            HEIGHT = CHUNK_SIZE * chunkCount / 2;
+        }
+
+    }
+
+    private void CreateChunks()
+    {
+        int _tileIndex = 0;
+        int nominalXValue;
+        int nominalYValue;
+
+        
+        for (int y = 0; y < HEIGHT; y += 8)
+        {
+            for (int x = 0; x < WIDTH; x += 8)
+            {
+                for (int m = 0; m < CHUNK_SIZE; m++)
+                {
+                    for (int n = 0; n < CHUNK_SIZE; n++)
+                    {
+                        _tiles.Add(Instantiate(_tilePrefab));
+
+                       // _tiles[_tileIndex].SetActive(true);
+
+                        if (x == 0 && y == 0)
+                        {
+                            nominalXValue = n;
+                            nominalYValue = m;
+                        }
+
+                        else
+                        {
+                            nominalXValue = x + m;
+                            nominalYValue = y + n;
+                        }
+
+                        if (m == 0 && n == 0)
+                        {
+                            _tiles[_tileIndex].name = nominalXValue + " " + nominalYValue + " Parent";
+                        }
+
+                        else if (m != 0 && n == 0)
+                        {
+                            _tiles[_tileIndex].name = nominalXValue + " " + nominalYValue + " Child";
+
+                            _tiles[_tileIndex].transform.SetParent(_tiles[_tileIndex - m].transform);
+                        }
+
+                        else if (m == 0 && n != 0)
+                        {
+                            _tiles[_tileIndex].name = nominalXValue + " " + nominalYValue + " Child";
+
+                            _tiles[_tileIndex].transform.SetParent(_tiles[_tileIndex - n].transform);
+                        }
+
+                        else if (m != 0 && n != 0)
+                        {
+                            _tiles[_tileIndex].name = nominalXValue + " " + nominalYValue + " Child";
+
+                            _tiles[_tileIndex].transform.SetParent(_tiles[_tileIndex - n - m].transform);
+                        }
+
+                        _tiles[_tileIndex].tileType.RetrieveTileType(_tiles[_tileIndex], x + n, y + m, scale, heightMultiplier, octaves, persistance, lacunarity, xOffset, yOffset);
+
+                        float xPos = x + n - WIDTH / 2 + 0.5f;
+                        float yPos = y + m - HEIGHT / 2 + 0.5f;
+
+                        _tiles[_tileIndex].transform.Translate(new Vector2(xPos, yPos));
+
+                        _tileIndex++;
+
+                        TILE_COUNT++;
+                    }
+                }
             }
         }
     }
