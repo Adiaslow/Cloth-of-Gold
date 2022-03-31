b39fdb47ac229cddd6b576d809fc8afd6260db4f
Adam Murray <a.murray0413@gmail.com>
2022-03-31 10:20:18 -0700

    Push

---
+++ Cloth-of-Gold 0.0.5/Assets/Scripts/SOs/BaseClass/RootCursor_SO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "New Root Cursor", menuName = "CoG/New Root Cursor")]
+public class RootCursor_SO : ScriptableObject
+{
+	public List<TileBase> cursors;
+
+	public TileBase getRndCursor() => cursors[UnityEngine.Random.Range(0, cursors.Count)];
+}
--- Cloth-of-Gold 0.0.5/Assets/TakuScripts/SOs/BaseClass/RootTile_SO.cs
+++ Cloth-of-Gold 0.0.5/Assets/TakuScripts/SOs/BaseClass/RootTile_SO.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+
 [CreateAssetMenu(fileName = "New Root Tile", menuName = "CoG/New Root Tile")]
 public class RootTile_SO : ScriptableObject
 {
 	public List<TileBase> tiles;
-
-	public TileBase getRndTile() => tiles[UnityEngine.Random.Range(0, tiles.Count)];
 }
